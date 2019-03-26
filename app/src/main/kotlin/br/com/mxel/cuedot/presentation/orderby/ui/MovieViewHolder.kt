443812d40d4c9b96ab0d1006cc946ac9c6fbbb66
Michel Ribeiro <michelrbr@gmail.com>
2019-03-26 19:17:19 -0300

    Use Butterknife to views at MovieViewHolder

--- app/src/main/kotlin/br/com/mxel/cuedot/presentation/orderby/ui/MovieViewHolder.kt
+++ app/src/main/kotlin/br/com/mxel/cuedot/presentation/orderby/ui/MovieViewHolder.kt
@@ -2,26 +2,44 @@ package br.com.mxel.cuedot.presentation.orderby.ui
 
 import android.view.View
 import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatRatingBar
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.mxel.cuedot.R
 import br.com.mxel.cuedot.domain.entity.Movie
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.squareup.picasso.Picasso
 
 class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    private var currentMovie: Movie? = null
+    private var _currentMovie: Movie? = null
+    val currentMovie: Movie?
+        get() = _currentMovie
+
+    // UI
+    @BindView(R.id.movie_title)
+    lateinit var title: AppCompatTextView
+    @BindView(R.id.release_date_text_view)
+    lateinit var date: AppCompatTextView
+    @BindView(R.id.rating)
+    lateinit var ratingBar: AppCompatRatingBar
+    @BindView(R.id.movie_cover)
+    lateinit var movieCover: ImageView
+
+    init {
+        ButterKnife.bind(this, view)
+    }
 
     fun setMovie(movie: Movie) {
 
-        currentMovie = movie
-        (itemView.findViewById(R.id.movie_title) as TextView).text = movie.title
-        (itemView.findViewById(R.id.release_date_text_view) as TextView).text = movie.releaseDate
-        (itemView.findViewById(R.id.rating) as RatingBar).rating = movie.voteAverage!! / 2
+        _currentMovie = movie
+        title.text = movie.title
+        date.text = movie.releaseDate
+        ratingBar.rating = movie.voteAverage!! / 2
         Picasso.get()
                 .load(movie.posterPath)
-                .into(itemView.findViewById(R.id.movie_cover) as ImageView)
+                .into(movieCover)
     }
 }
 
