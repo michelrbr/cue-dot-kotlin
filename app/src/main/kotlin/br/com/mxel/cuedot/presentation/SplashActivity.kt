ec2cf2f0e6f391eccb89b7f38d1f289981f14980
Michel Ribeiro <michelrbr@gmail.com>
2019-04-09 19:58:02 -0300

    App: Create theme for SplashActivity

--- app/src/main/kotlin/br/com/mxel/cuedot/presentation/SplashActivity.kt
+++ app/src/main/kotlin/br/com/mxel/cuedot/presentation/SplashActivity.kt
@@ -1,16 +1,15 @@
 package br.com.mxel.cuedot.presentation
 
 import android.os.Bundle
-import br.com.mxel.cuedot.R
 import br.com.mxel.cuedot.domain.orderby.Order
 import br.com.mxel.cuedot.presentation.base.BaseActivity
 import br.com.mxel.cuedot.presentation.orderby.OrderedByActivity
 
-class MainActivity : BaseActivity() {
+class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         OrderedByActivity.launch(this, Order.POPULAR)
     }
