3fa7bcb373e639c59d24c6c8d1f93bcd35ba0ce6
Michel Ribeiro <michelrbr@gmail.com>
2019-04-19 15:47:42 -0300

    App: Add leak canary lib. Upgrade Kotlin and Gradle plugin version

--- app/src/main/kotlin/br/com/mxel/cuedot/CueDotApplication.kt
+++ app/src/main/kotlin/br/com/mxel/cuedot/CueDotApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import br.com.mxel.cuedot.di.appModule
 import br.com.mxel.cuedot.di.networkModule
 import br.com.mxel.cuedot.di.orderByModule
+import com.squareup.leakcanary.LeakCanary
 import org.koin.android.ext.android.startKoin
 import timber.log.Timber
 
@@ -11,6 +12,12 @@ class CueDotApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
